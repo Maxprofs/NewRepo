8776484cedb8aabb032b38ed793caa71e9b42f46
kashifakhan <kashifakhan@cedcoss.com>
2017-08-24 11:24:50 +0530

    file saperated

---
+++ Admin/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header page</title>
+	<link rel="stylesheet" href="resources/css/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="resources/css/style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="resources/css/invalid.css" type="text/css" media="screen" />	
+<script type="text/javascript" src="resources/scripts/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="resources/scripts/simpla.jquery.configuration.js"></script>
+<script type="text/javascript" src="resources/scripts/facebox.js"></script>
+<script type="text/javascript" src="resources/scripts/jquery.wysiwyg.js"></script>
+<script type="text/javascript" src="resources/scripts/jquery.datePicker.js"></script>
+<script type="text/javascript" src="resources/scripts/jquery.date.js"></script>
+</head>
+<body>
+
+</body>
+</html>
